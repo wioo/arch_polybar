ebb4d98be4f096052b919a5bffcfdd84c982dd17
Blaz <wioo@users.noreply.github.com>
2018-01-14 11:28:34 +0100

    Update arch_update_once.sh

--- scripts/arch/arch_update_once.sh
+++ scripts/arch/arch_update_once.sh
@@ -14,18 +14,18 @@ else
 fi
 
 if [ "$nb_arch" -ne "0" ]; then
-	if [ "$nb_aur" -eq "0" ];then
-	checkupdates >> ~/.config/polybar/scripts/arch/packages
+        if [ "$nb_aur" -eq "0" ];then
+        checkupdates >> ~/.config/polybar/scripts/arch/packages
       else
         checkupdates >> ~/.config/polybar/scripts/arch/packages
-	echo -e "\nAUR updates" >> ~/.config/polybar/scripts/arch/packages
+        echo -e "\nAUR updates" >> ~/.config/polybar/scripts/arch/packages
         trizen -Su --aur --quiet >> ~/.config/polybar/scripts/arch/packages
        fi
 else
 if [ "$nb_aur" -eq "0" ];then
         echo "" > /dev/null
         else
-        echo -e "AUR updates" > ~/.config/polybar/scripts/arch/packages
+        echo "AUR updates" > ~/.config/polybar/scripts/arch/packages
         trizen -Su --aur --quiet >> ~/.config/polybar/scripts/arch/packages
        fi
 fi
