cdf09ef3b0758cebe4e0c79c45cf04e4d86c816c
Blaz <wioo@users.noreply.github.com>
2018-01-21 19:45:58 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
   
-# Terminate already running bar instances
-
-#kill all scripts (arch linux updates)
+#kill all scripts (arch_updates)
 kill -9 -$(ps -o pgid -p $(cat ~/.config/polybar/scripts/arch/arch_updates.pid) | tail -n1 | sed 's/^[ \t]*//;s/[ \t]*$//')
 kill -9 $(cat ~/.config/polybar/scripts/arch/polybar_updates.pid)
 
-#killall -q polybar
+# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
@@ -15,6 +13,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar example &
 
-# Launch arch updates script
+# Launch arch_updates script
 arch_updates & echo $! > ~/.config/polybar/scripts/arch/arch_updates.pid
 echo "Bars launched..."
