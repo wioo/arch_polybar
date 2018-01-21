369d2aec452e79cb86258c0e06127db4696d88c6
Blaz <wioo@users.noreply.github.com>
2018-01-21 19:38:58 +0100

    Create arch_updates.sh

---
+++ scripts/arch/arch_updates.sh
@@ -0,0 +1,53 @@
+#!/bin/env bash
+  
+path=${HOME}/.config/polybar/scripts/arch/
+
+function main_loop {
+        while true; do
+        check_for_updates
+        sleep 600
+        done
+}
+
+function status {
+        echo $$ > ${path}polybar_updates.pid
+        while true; do
+        cat ~/.config/polybar/scripts/arch/status
+        sleep 1
+        done
+}
+
+function check_for_updates {
+   checkupdates | nl -w2 -s '. ' >| ${path}repo.pkgs
+   trizen -Su --aur --quiet | sed 's/://;s/==/-/' >| ${path}aur.pkgs
+   updates=$(cat ${path}*.pkgs | wc -l)
+
+   echo "0" >| ${path}status
+   [ $updates -gt 0 ] && echo "%{F#e60053}$updates" >| ${path}status
+
+   >| ${path}packages
+   [ -s ${path}repo.pkgs ] && cat ${path}repo.pkgs >> ${path}packages
+   [ -s ${path}repo.pkgs ] && [ -s ${path}aur.pkgs ] && printf "\n" >> ${path}packages
+   [ -s ${path}aur.pkgs ] && sed '1iAUR Updates' ${path}aur.pkgs >> ${path}packages
+}
+
+function notify {
+if [[ $(cat ~/.config/polybar/scripts/arch/status) -eq 0 ]]
+then
+        notify-send 0
+else
+        notify-send "$(cat .config/polybar/scripts/arch/packages)"
+fi
+}
+
+function upgrade {
+urxvt -e trizen --noconfirm -Syu
+echo "0" > ~/.config/polybar/scripts/arch/status
+}
+
+
+[[ $# -eq 0 ]] && main_loop
+[[ $1 == "-s" ]] && status
+[[ $1 == "-c" ]] && echo '' > ~/.config/polybar/scripts/arch/status && check_for_updates
+[[ $1 == "-n" ]] && notify
+[[ $1 == "-u" ]] && upgrade
